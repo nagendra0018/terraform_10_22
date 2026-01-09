18e85fed327986e414fdaa4abeee384855913c56
Gorle <ngorle@netapp.com>
2026-01-09 13:05:47 +0530

    add dynamo state locking

--- Day-4-State-locking/backend.tf
+++ Day-4-State-locking/backend.tf
@@ -3,6 +3,9 @@ terraform {
     bucket = "nagendraindhuja143"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true
+    #use_lockfile = true >1.19 version above we can use s3 state locking
+    dynamodb_table = "nagendra"
+    encrypt = true
+    
   }
 }
